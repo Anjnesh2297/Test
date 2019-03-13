8de67fd64f73ce93f50da4dd8d6d6ed66f7de543
Anjnesh2297 <48524279+Anjnesh2297@users.noreply.github.com>
2019-03-13 10:26:07 -0500

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,16 @@
+class Test{
+
+public void test(){
+  boolean clearSessionValueFlag="Test".equalsIgnoreCase("Test"); 
+			if(clearSessionValueFlag){
+				System.out.println("Yes in if");
+			}else{
+      System.out.println("Yes in else");
+      }
+			if("Test2".equalsIgnoreCase("Test1")){
+				System.out.println("Yes in if");
+			}else{
+        System.out.println("Yes in else");
+      }
+      }
+    }
